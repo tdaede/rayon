e81d8656720c58548bd304f2355ce860e21f1b19
Niko Matsakis <niko@alum.mit.edu>
2019-11-03 10:36:14 -0500

    remove pure "debug" events; this lowers overhead of enabling logging

--- rayon-core/src/registry.rs
+++ rayon-core/src/registry.rs
@@ -700,7 +700,6 @@ impl WorkerThread {
         // wait.
         self.registry.sleep.work_found(idle_state);
 
-        self.log(|| ThreadSawLatchSet { worker: self.index });
         mem::forget(abort_guard); // successful execution, do not abort
     }
 
